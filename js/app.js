0456d095a717583a8c0d2f827d070da6d08bad4c
unknown <aponasifaman@gmail.com>
2024-02-07 15:20:25 +0600

    second-commit

--- js/app.js
+++ js/app.js
@@ -1,13 +1,11 @@
 // Do not show API key on your JS file like this
 const API_KEY = `9b6d70d636acfa62e7fdddf965ac50d4`;
 
-
-const loadTemperature = city =>{
+const loadTemperature = city => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
     fetch(url)
-    .then(res => res.json())
-    .then(data => console.log(data));
-
+        .then(res => res.json())
+        .then(data => console.log(data));
 }
 
-loadTemperature('dhaka');
+loadTemperature('dhaka'); 
